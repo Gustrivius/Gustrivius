4d4b8d8e004d044bfd0c62adb852bf8bd965ce63
mwynia <BubbleButtben10$>
2023-04-11 13:12:02 -0500

    Menu button in QA

--- app/src/main/java/com/example/gustrivius/QAActivity.kt
+++ app/src/main/java/com/example/gustrivius/QAActivity.kt
@@ -1,15 +1,26 @@
 package com.example.gustrivius
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.gustrivius.databinding.ActivityQaactivityBinding
 
 class QAActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQaactivityBinding
+    private val MenuLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {result ->}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQaactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.MenuButton.setOnClickListener { view: View ->
+            val intent = Intent(this, QAActivity::class.java)
+            MenuLauncher.launch(intent)
+        }
     }
 }

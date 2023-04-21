6d7e80dfb5c9afdc4350aa0ab0f6c01d7ec1619b
Wenleiyu <wyu2@gustavus.edu>
2023-04-20 22:23:12 -0500

    score update and moving to next questions functions work successfully!

--- app/src/main/java/com/example/gustrivius/playActivity.kt
+++ app/src/main/java/com/example/gustrivius/playActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import com.example.gustrivius.databinding.ActivityPlayBinding
 import com.example.gustrivius.databinding.ActivityQaactivityBinding
 import com.google.firebase.firestore.FirebaseFirestore
@@ -14,6 +15,7 @@ import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 var QID = ArrayList<String>()
 var correct_score = 0
+var click= 0;
 
 class playActivity : AppCompatActivity()  {
     val db = Firebase.firestore
@@ -29,7 +31,7 @@ class playActivity : AppCompatActivity()  {
         setContentView(binding.root)
 
         var ids = intent.getSerializableExtra("questionID") as ArrayList<String>
-        for (i in 0..(ids.size - 1)){
+        for (i in 0..(ids.size - 1)) {
             QID.add(ids[i])
         }
         QID.shuffle()
@@ -37,63 +39,89 @@ class playActivity : AppCompatActivity()  {
         //var questionTextView = findViewById(R.id.question_text);
 
         //All the code for getting data from Firebase
-        val doc = db.collection("questions").document(QID[0])
+        //while(true) {
+        moveToNext();
+    }
+    fun moveToNext() {
+        if (click == QID.size) {
+            click--;
+            AlertDialog.Builder (this)
+                .setTitle("Done")
+                .setMessage("Congratulation, you answered all the questions, the score is: $correct_score")
+                .setPositiveButton("Back to menu") {dialogInterface, i -> finish()}
+                .setCancelable(false)
+                .show()
+        }
+        val doc = db.collection("questions").document(QID[click])
         FirebaseFirestore.getInstance().collection("questions").get()
         doc.get().addOnSuccessListener { DocumentSnapShot ->
-        //val questions = documentSnapshot.toObject<questions>()
+            //val questions = documentSnapshot.toObject<questions>()
             if (DocumentSnapShot != null) {
-                val answerChoice = arrayOf(DocumentSnapShot.get("correctAnswer").toString(), DocumentSnapShot.get("wrongAnswer1").toString(), DocumentSnapShot.get("wrongAnswer2").toString(), DocumentSnapShot.get("wrongAnswer3").toString())
+                val answerChoice = arrayOf(
+                    DocumentSnapShot.get("correctAnswer").toString(),
+                    DocumentSnapShot.get("wrongAnswer1").toString(),
+                    DocumentSnapShot.get("wrongAnswer2").toString(),
+                    DocumentSnapShot.get("wrongAnswer3").toString()
+                )
                 answerChoice.shuffle()
                 binding.questionText.text = DocumentSnapShot.get("text").toString()
                 binding.choice1.text = answerChoice[0]
                 binding.choice2.text = answerChoice[1]
                 binding.choice3.text = answerChoice[2]
                 binding.choice4.text = answerChoice[3]
-            //val obj = document.data.toObject<questions>()
-            //Toast.makeText(this, DocumentSnapShot.get("text").toString(), Toast.LENGTH_SHORT).show()
+                //val obj = document.data.toObject<questions>()
+                //Toast.makeText(this, DocumentSnapShot.get("text").toString(), Toast.LENGTH_SHORT).show()
                 binding.choice1.setOnClickListener { view: View ->
-                    if (binding.choice1.text == DocumentSnapShot.get("correctAnswer").toString()) {
+                    if (binding.choice1.text == DocumentSnapShot.get("correctAnswer")
+                            .toString()
+                    ) {
                         correct_score++;
                         binding.score.text = correct_score.toString();
                     }
+                    click++;
+                    moveToNext()
                 }
 
                 binding.choice2.setOnClickListener { view: View ->
-                    if (binding.choice2.text == DocumentSnapShot.get("correctAnswer").toString()) {
+                    if (binding.choice2.text == DocumentSnapShot.get("correctAnswer")
+                            .toString()
+                    ) {
                         correct_score++;
                         binding.score.text = correct_score.toString();
                     }
+                    click++;
+                    moveToNext()
                 }
 
                 binding.choice3.setOnClickListener { view: View ->
-                    if (binding.choice3.text == DocumentSnapShot.get("correctAnswer").toString()) {
+                    if (binding.choice3.text == DocumentSnapShot.get("correctAnswer")
+                            .toString()
+                    ) {
                         correct_score++;
                         binding.score.text = correct_score.toString();
                     }
+                    click++;
+                    moveToNext()
                 }
 
                 binding.choice4.setOnClickListener { view: View ->
-                    if (binding.choice4.text == DocumentSnapShot.get("correctAnswer").toString()) {
+                    if (binding.choice4.text == DocumentSnapShot.get("correctAnswer")
+                            .toString()
+                    ) {
                         correct_score++;
                         binding.score.text = correct_score.toString();
                     }
+                    click++;
+                    moveToNext()
                 }
             }
-        }.addOnFailureListener { exception -> Toast.makeText(this, "error", Toast.LENGTH_SHORT).show() }
-
+        }.addOnFailureListener { exception ->
+            Toast.makeText(
+                this,
+                "error",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
-
-    /* disable all the buttons if we click any one answer
-    private fun updateQuestion(){
-        val questionTextResId = currentQuestionText
-        if (quizViewModel.currentQuestionAnswered == true) {
-            binding.trueButton.isEnabled = false
-            binding.falseButton.isEnabled = false
-        }
-        else{
-            binding.trueButton.isEnabled = true
-            binding.falseButton.isEnabled = true
-        }
-    }*/
 
 }
